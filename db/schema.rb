5b2caa69d62d78d0978f2633a25f0f02ea757824
talk2price <chaudhary.prince@sitpune.edu.in>
2020-05-19 16:21:47 +0530

    heroku commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2020_05_18_111508) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -19,11 +26,6 @@ ActiveRecord::Schema.define(version: 2020_05_18_111508) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+
 
 end
